6574cddc609d676ceaf370bf558fc0f3ee66dc21
Trang Do <dhthutrang@gmail.com>
2020-08-25 14:08:54 +0200

    Test count_exon.sh

--- mRNA_seq/script/count_exon.sh
+++ mRNA_seq/script/count_exon.sh
@@ -1,38 +1,33 @@
 echo "=====> Begin counting"
 cd "$(dirname "$0")"
-
-if (test -f mRNA_seq_BEDaccessionnumber.txt && test -f epi_ids.txt)
-then
-    echo "...File requirement met."
-else
-    echo "Err: Check mRNA_seq_BEDaccessionnumber.txt"
-    exit 1
-fi
+mkdir $ENCODE_EXP/dexseqcount #dexseqcount instead of backupcount_NCBI
 
 # COUNTING =====================================
 dexseq_count() {
-    f="$p".sam
-    ACC_NO=${f%%.*}
-    echo "counting $ACC_NO" >> log_dexseqcount.txt
-    (python ~/R/x86_64-pc-linux-gnu-library/3.6/DEXSeq/python_scripts/dexseq_count.py $REFGEN/reference_genome.gtf $f backupcount_NCBI/"$ACC_NO"_count.txt) || (echo "Err: Counting $ACC_NO failed" >> log_dexseqcount.txt)
+    ID=${file%%.*}
+    ID=${ID##*/}
+    echo $ID
+    echo $file
+    echo "counting $ID" >> log_dexseqcount.txt
+    # (python ~/R/x86_64-pc-linux-gnu-library/3.6/DEXSeq/python_scripts/dexseq_count.py $ENCODE_REFGEN/reference_genome.gtf $file $ENCODE_EXP/dexseqcount/"$ID"_count.txt) || (echo "Err: Counting $ID failed" >> log_dexseqcount.txt)
 }
-
-while read p; 
+for file in ls $ENCODE_EXP/sam_files/*.sam
 do
     dexseq_count &
-done < mRNA_seq_BEDaccessionnumber.txt #temp.txt is accession_number (with order sometimes)
-wait
+done
+wait 
 
 echo "=====> Changing names"
+
+
 # Change name to contain epigenome ID =====================================
-for f in backupcount_NCBI/*count.txt
+for f in $ENCODE_EXP/dexseqcount/*count.txt
 do
     ACC_NO=${f#*/}
     ACC_NO=${ACC_NO%_*}
-    ID=$(grep -ih $ACC_NO epi_ids.txt)
-    prefix_ID=${ID%%_*}
-    NEWNAME="backupcount_NCBI"/"${prefix_ID}"_"$ACC_NO"_"count.txt"
-    mv $f $NEWNAME
+    # ID=$(grep -ih $ACC_NO epi_ids.txt)
+    # prefix_ID=${ID%%_*}
+    # NEWNAME="backupcount_NCBI"/"${prefix_ID}"_"$ACC_NO"_"count.txt"
+    # mv $f $NEWNAME
 done
-
 echo "=====> Finished counting"
