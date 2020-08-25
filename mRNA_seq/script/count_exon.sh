0f2a8bac280f59a7f6e7ea895a928ba1a5347a93
Trang Do <dhthutrang@gmail.com>
2020-08-25 22:31:39 +0200

    Edit count_exon.sh

--- mRNA_seq/script/count_exon.sh
+++ mRNA_seq/script/count_exon.sh
@@ -8,8 +8,13 @@ dexseq_count() {
     ID=${ID##*/}
     echo $ID
     echo $file
-    echo "counting $ID" >> log_dexseqcount.txt
-    (python ~/R/x86_64-pc-linux-gnu-library/3.6/DEXSeq/python_scripts/dexseq_count.py $ENCODE_REFGEN/reference_genome.gtf $file $ENCODE_EXP/dexseqcount/"$ID"_count.txt) || (echo "Err: Counting $ID failed" >> log_dexseqcount.txt)
+    echo "Counting $ID" >> log_dexseqcount.txt
+    if [ -e "$ENCODE_EXP/dexseqcount/"$ID"_count.txt" ]
+    then 
+        echo "ALready counted $ID" >> log_dexseqcount.txt    
+    else
+        (python ~/R/x86_64-pc-linux-gnu-library/3.6/DEXSeq/python_scripts/dexseq_count.py $ENCODE_REFGEN/reference_genome.gtf $file $ENCODE_EXP/dexseqcount/"$ID"_count.txt) || (echo "Err: Counting $ID failed" >> log_dexseqcount.txt)
+    fi
 }
 for file in ls $ENCODE_EXP/sam_files/*.sam
 do
