53fe634e54f8527b270411b37d598ba294b82f43
Trang Do <dhthutrang@gmail.com>
2020-08-25 12:28:10 +0200

    Edit execute_manorm.sh

--- chip_seq/H3K27ac/execute_manorm.sh
+++ chip_seq/H3K27ac/execute_manorm.sh
@@ -12,19 +12,14 @@ execute_manorm_parallel() {
     read_file2=$(echo $p | awk '{split($0, a, " "); print a[4]}')
     epi1=$(echo $p | awk '{split($0, a, " "); print a[5]}')
     epi2=$(echo $p | awk '{split($0, a, " "); print a[6]}')
-    if (ls . | grep "$epi1" ) && (ls $f | grep "$epi2" )
+    if (ls manorm_result | grep "$epi1"_"$epi2" )
     then (
-        if (ls manorm_result | grep "$epi1"_"$epi2" )
-        then (
-            echo "Pair "$epi1"_"$epi2" already exists" >> execute_manorm.log
+        echo "Pair "$epi1"_"$epi2" already exists" >> execute_manorm.log
         )
-        else (
-            echo "MAnorm-ing pair "$epi1"_"$epi2": "$peak_file1"|"$peak_file2"|"$read_file1"|"$read_file2"" >> execute_manorm.log
-            manorm --p1 "$peak_file1" --p2 "$peak_file2" --r1 "$read_file1" --r2 "$read_file2" -o manorm_result/"$epi1"_"$epi2"
+    else (
+        echo "MAnorm-ing pair "$epi1"_"$epi2": "$peak_file1"|"$peak_file2"|"$read_file1"|"$read_file2"" >> execute_manorm.log
+        manorm --p1 "$peak_file1" --p2 "$peak_file2" --r1 "$read_file1" --r2 "$read_file2" -o manorm_result/"$epi1"_"$epi2"
         )
-        fi
-        )
-    else echo "Pair "$epi1"_"$epi2" does not exist" >> execute_manorm.log
     fi
 }
 
