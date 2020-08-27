2f8ab49d3ba8d988ee9c3d5e33c66b778ecd7550
Trang Do <dhthutrang@gmail.com>
2020-08-27 11:39:05 +0200

    Edit get_flank_gtf.R

--- refgen/get_flank_gtf.R
+++ refgen/get_flank_gtf.R
@@ -3,31 +3,33 @@ library(rtracklayer)
 library(data.table)
 library(dplyr)
 
-refgen.exon = import.gff("/home/dhthutrang/ENCODE/refgen/reference_genome.exon.gtf")
+refgen.exon = import.gff("/Users/dhthutrang/Documents/BIOINFO/Episplicing/ENCODE_episplicing/refgen/reference_genome.gtf")
 refgen.exon = as(refgen.exon, "GRanges")
 start(refgen.exon)
 strand(refgen.exon)
-refgen.exon$transcripts
+refgen.exon$transcript_id
+refgen.exon = refgen.exon[refgen.exon$type == "exonic_part"]
 
 refgen.flank.start <- GRanges(
   seqnames = seqnames(refgen.exon),
   ranges = IRanges(start = start(refgen.exon)-200, width = 200*2),
   strand = strand(refgen.exon),
   type = "flank_start",
-  gene = refgen.exon$gene_id,
-  transcript = refgen.exon$transcripts,
-  exon = paste("E", refgen.exon$exonic_part_number, sep='')
+  gene_id = refgen.exon$gene_id,
+  transcript_id = refgen.exon$transcript_id,
+  exonic_part_number = paste("E", refgen.exon$exonic_part_number, sep='')
 )
 refgen.flank.end <- GRanges(
   seqnames = seqnames(refgen.exon),
   ranges = IRanges(start = end(refgen.exon)-200, width = 200*2),
   strand = strand(refgen.exon),
   type = "flank_end",
-  gene = refgen.exon$gene_id,
-  transcript = refgen.exon$transcripts,
-  exon = paste("E", refgen.exon$exonic_part_number, sep='')
+  gene_id = refgen.exon$gene_id,
+  transcript_id = refgen.exon$transcript_id,
+  exonic_part_number = paste("E", refgen.exon$exonic_part_number, sep='')
 )
 refgen.flank = c(refgen.flank.start, refgen.flank.end)
+head(refgen.flank)
 min(start(refgen.flank))
 start(refgen.flank[start(refgen.flank) < 0]) = 1
 min(start(refgen.flank))
