1c513ba9ecd6bc8d687fe56575ea5d62e00e0c03
Trang Do <dhthutrang@gmail.com>
2020-08-27 11:13:27 +0200

    Daily push

--- refgen/get_flank_gtf.R
+++ refgen/get_flank_gtf.R
@@ -3,7 +3,7 @@ library(rtracklayer)
 library(data.table)
 library(dplyr)
 
-refgen.exon = import.gff("/Users/dhthutrang/Documents/BIOINFO/Episplicing/files/gene_level/reference_genome.exon.gtf")
+refgen.exon = import.gff("/home/dhthutrang/ENCODE/refgen/reference_genome.exon.gtf")
 refgen.exon = as(refgen.exon, "GRanges")
 start(refgen.exon)
 strand(refgen.exon)
@@ -11,7 +11,7 @@ refgen.exon$transcripts
 
 refgen.flank.start <- GRanges(
   seqnames = seqnames(refgen.exon),
-  ranges = IRanges(start = start(refgen.exon)-100, width = 100*2),
+  ranges = IRanges(start = start(refgen.exon)-200, width = 200*2),
   strand = strand(refgen.exon),
   type = "flank_start",
   gene = refgen.exon$gene_id,
@@ -20,7 +20,7 @@ refgen.flank.start <- GRanges(
 )
 refgen.flank.end <- GRanges(
   seqnames = seqnames(refgen.exon),
-  ranges = IRanges(start = end(refgen.exon)-100, width = 100*2),
+  ranges = IRanges(start = end(refgen.exon)-200, width = 200*2),
   strand = strand(refgen.exon),
   type = "flank_end",
   gene = refgen.exon$gene_id,
@@ -30,11 +30,11 @@ refgen.flank.end <- GRanges(
 refgen.flank = c(refgen.flank.start, refgen.flank.end)
 min(start(refgen.flank))
 start(refgen.flank[start(refgen.flank) < 0]) = 1
+min(start(refgen.flank))
 temp = sortSeqlevels(refgen.flank)
 temp = sort(temp)
-# temp = as.data.frame(temp)
 
-export(temp, "reference_genome.fl100.gtf")
+export(temp, "reference_genome.fl200.gtf")
 
 
 
