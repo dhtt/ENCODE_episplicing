174fb62d0a85c453034cc64d8bc472fc21a93146
dhtt_uni <dhthutrang@giga1.bioinformatik.uni-saarland.de>
2020-08-30 11:28:16 +0200

    Daily push

---
+++ mRNA_seq/script/get_all_pairs.sh
@@ -0,0 +1,7 @@
+# All subject tissues ID are stored in $REFGEN/epi_ids_list.main.txt
+awk '{ a[NR]=$0 }
+       END{ for(i=1;i<=NR;i++)
+              for(j=i+1;j<=NR;j++)
+		      if (a[i] != a[j]) print a[i], a[j] }' $ENCODE_Escript/epi_ids_list.main.txt > all_pairs.txt
+mkdir $ENCODE_Escript/pair_chunks
+split -l 11 --numeric-suffixes $ENCODE_Escript/all_pairs.txt $ENCODE_Escript/pair_chunks/chunk_
