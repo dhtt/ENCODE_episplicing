a8c2f39671d9758c8bccf4c4dc0fed549f22ec64
dhtt_uni <dhthutrang@giga1.bioinformatik.uni-saarland.de>
2020-09-17 18:49:13 +0200

    Update SCRIPTS

--- mRNA_seq/script/get_all_pairs.sh
+++ mRNA_seq/script/get_all_pairs.sh
@@ -2,6 +2,6 @@
 awk '{ a[NR]=$0 }
        END{ for(i=1;i<=NR;i++)
               for(j=i+1;j<=NR;j++)
-		      if (a[i] != a[j]) print a[i], a[j] }' $ENCODE_Escript/epi_ids_list.main.txt > all_pairs.txt
+		      if (a[i] != a[j]) print a[i], a[j] }' $ENCODE_Escript/epi_ids.txt > all_pairs.txt
 mkdir $ENCODE_Escript/pair_chunks
 split -l 11 --numeric-suffixes $ENCODE_Escript/all_pairs.txt $ENCODE_Escript/pair_chunks/chunk_
