172d38a97bcdc3d782783edd1045b19b500df068
Trang Do <dhthutrang@gmail.com>
2020-09-18 13:00:04 +0200

    Update all_pairs.txt, prepare_all_pairs.R, epi_ids.txt

--- mRNA_seq/script/prepare_all_pairs.R
+++ mRNA_seq/script/prepare_all_pairs.R
@@ -13,10 +13,12 @@ submeta = mrna_metadata %>%
 	 rename = paste('mv ', paste(sam_folder, '/', `File accession`, '.sam', sep=''), filename)
          ) %>%
   dplyr::select(rename)
-fwrite(submeta, "/home/dhthutrang/ENCODE/mRNA_seq/script/renameSAMfile.sh", row.names = FALSE, col.names = FALSE, quote = FALSE)
+fwrite(submeta, "/home/dhthutrang/ENCODE/mRNA_seq/script/renameSAMfiles.sh", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 tissue = mrna_metadata$`Biosample term name`
 tissue = sapply(tissue, function(x) gsub('[[:punct:]]|\ ', '', x))
+tissue = tissue[order(tissue)]
 tissue = as.data.frame(unique(tissue))
-fwrite(submeta, "/home/dhthutrang/ENCODE/mRNA_seq/script/epi_ids.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+# fwrite(tissue, "/home/dhthutrang/ENCODE/mRNA_seq/script/epi_ids.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
+fwrite(tissue, "/Users/dhthutrang/Documents/BIOINFO/Episplicing/ENCODE_episplicing/mRNA_seq/script/epi_ids.txt", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
