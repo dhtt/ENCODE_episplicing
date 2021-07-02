d031cc8f884dc80f27e560ac8228ebc2e79fe4a5
trangdht <trang@ki-elements.de>
2021-07-02 12:47:39 +0200

    change majiq_count_AS.sh

--- mRNA_seq/script/majiq_count_AS.sh
+++ mRNA_seq/script/majiq_count_AS.sh
@@ -1,1 +1,10 @@
-awk -F';' '{print $5}' psoasmuscle_trophoblastcell.deltapsi.tsv| grep -c True 
+for f in majiq_to_be_mapped/*
+do
+    NAME=${f##*/}
+    NAME=${NAME%%.*}
+    echo $f $NAME
+    echo $NAME >> majiq_all_AS.txt
+    awk -F';' '{print $5}' $f | grep -c True >> majiq_all_AS.txt
+    awk -F';' '{print $6}' $f | grep -c True >> majiq_all_AS.txt
+    awk -F';' '{print $7}' $f | grep -c True >> majiq_all_AS.txt
+done
