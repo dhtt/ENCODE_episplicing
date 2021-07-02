e297de5e297ac3239d57cf7b603f8e6224b0e852
dhtt_uni <dhthutrang@giga1.bioinformatik.uni-saarland.de>
2021-07-02 18:29:28 +0200

    Add majiq_annotate_trimmed.sh

--- mRNA_seq/script/majiq_annotate_trimmed.sh
+++ mRNA_seq/script/majiq_annotate_trimmed.sh
@@ -7,11 +7,23 @@ do
     NAME=${FILE##*/}
     NAME=${NAME%%.*}
     OUTPUT=$MAJIQ_annotated_histone'/'$NAME'.bed'
-    HIS_ANNOT='/home/dhthutrang/ENCODE/chip_seq/'$H'/flank/'$NAME'.txt.fl.txt'
+    HIS_ANNOT='/home/dhthutrang/ENCODE/chip_seq/'$H'/flank/bed/'$NAME'.bed'
     echo $H $HIS_ANNOT $OUTPUT 
     bedtools intersect -a $HIS_ANNOT -b $FILE -wo -loj -bed > $OUTPUT
 done
 wait
 
 echo 'DONE'
+cd $MAJIQ_annotated_histone
+mkdir collapsed
+for f in *.bed
+do
+bedtools groupby -i $f -g 1-5 -c 7,8,11 -o collapse,collapse,collapse > collapsed/$f
+done
+echo "Finish"
+
+rm *.bed && mv collapsed/* ./ && rm -r collapsed
+find ./ -size  0 -print -delete
+wc -l *
+
 
