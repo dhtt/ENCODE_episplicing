f45cfd07d04ffc1a3dab8b011f2f9756139e801f
trangdht <trang@ki-elements.de>
2021-07-14 16:02:16 +0200

    Add plot CDF, change gene_correl and analyze_flank

---
+++ mRNA_seq/script/plot_CDF_mval.py
@@ -0,0 +1,45 @@
+from typing import List
+import pandas as pd
+import glob
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style("ticks")
+
+
+def flatten_list(input_list: List[list]):
+    return [item for sublist in input_list for item in sublist]
+
+
+if __name__ == "__main__":
+    histone_types = ['H3K27ac', 'H3K27me3', 'H3K36me3', 'H3K4me1', 'H3K4me3', 'H3K9me3']
+    histone_col = sns.color_palette("husl", 6)
+    path = '/home/dhthutrang/ENCODE/mRNA_seq/script/majiq_annotated/'  # use your path
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    all_his_data = []
+    for i, his in enumerate(histone_types):
+        all_files = glob.glob(path + his + "/*.bed")
+        his_col = histone_col[i]
+        his_data = []
+        for file in all_files:
+            data = pd.read_csv(file, header=None, sep=' ').iloc[:, 0].tolist()
+            his_data.append(data)
+            sns.ecdfplot(data=data, color=his_col, alpha=0.05)
+        all_his_data.append(flatten_list(his_data))
+
+    for i, his_data in enumerate(all_his_data):
+        his_col = histone_col[i]
+        print(his_data)
+        sns.ecdfplot(data=his_data, color=histone_col[i], alpha=0.9, label=i)
+
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.xlabel("M-value", fontsize=16)
+    plt.ylabel("Cumulative proportion", fontsize=16)
+
+    h, l = ax.get_legend_handles_labels()
+    plt.legend(handles=h, labels=histone_types, loc='lower right', title="Histone type")
+    plt.savefig('mval_cdf.tiff', dpi=300)
