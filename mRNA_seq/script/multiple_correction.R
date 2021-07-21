e8425ce01eccb7d4226dc2e9c1e042573a2a6c95
trangdht <trang@ki-elements.de>
2021-07-21 15:42:33 +0200

    multiple_correction.R

--- mRNA_seq/script/multiple_correction.R
+++ mRNA_seq/script/multiple_correction.R
@@ -1,1 +1,17 @@
-multiple
+library(data.table, quietly=TRUE)
+library(dplyr, quietly=TRUE)
+
+setwd("/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/res")
+all_files = list.files(pattern = '*.csv')
+
+
+all_DEU_genes = vector("list", length(all_files))
+for (i in length(all_files)){
+  file = all_files[i]
+  data = data.table(read.csv(file, sep='\t'))
+  all_DEU_genes[[i]] = unique(data[abs(data[[7]]) >= 2.0 & data[[6]] <= 0.05, 'groupID'][[1]])
+}
+  
+all_DEU_genes = reduce(all_DEU_genes, union)
+all_DEU_genes = paste(all_DEU_genes, collapse = '\|')
+print(all_DEU_genes)
