919084110f1de29640175ae9679ad8960992a968
dhtt_uni <dhthutrang@giga1.bioinformatik.uni-saarland.de>
2021-07-21 17:48:16 +0200

    Official multiple_correction.R

--- mRNA_seq/script/multiple_correction.R
+++ mRNA_seq/script/multiple_correction.R
@@ -9,16 +9,16 @@ for (i in 1:length(all_files)){
   file = all_files[i]
   print(file)
   data = data.table(read.csv(file, sep='\t'))
-  all_DEU_genes[[i]] = unique(data[abs(data[[7]]) >= 2.0 & data[[6]] <= 0.05, 'groupID'][[1]])
+  all_DEU_genes[[i]] = unique(data[abs(data[[7]]) >= 2.0 & data[[6]] <= 0.0001, 'groupID'][[1]])
 }
   
-all_DEU_genes = unique(Reduce(union, all_DEU_genes))
+all_DEU_genes = unique(Reduce(intersect, all_DEU_genes))
 all_DEU_genes = all_DEU_genes[grep('+', all_DEU_genes, fixed = TRUE, invert = TRUE)]
 all_DEU_genes = all_DEU_genes[grep('_', all_DEU_genes, fixed = TRUE, invert = TRUE)]
 
 print('Before intersect: ')
 print(length(all_DEU_genes))
-ref_genes = readRDS('all_genes.2021.RDS') 
+ref_genes = readRDS('/home/dhthutrang/ENCODE/mRNA_seq/script/all_genes.2021.RDS') 
 all_DEU_genes = intersect(all_DEU_genes, ref_genes)
 print('After intersect: ')
 print(length(all_DEU_genes))
