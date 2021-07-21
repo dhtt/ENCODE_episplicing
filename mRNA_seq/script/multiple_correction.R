de359882ab32caa0aacf4968b57f95e43336fa8b
trangdht <trang@ki-elements.de>
2021-07-21 15:47:12 +0200

    multiple_correction.R

--- mRNA_seq/script/multiple_correction.R
+++ mRNA_seq/script/multiple_correction.R
@@ -8,10 +8,11 @@ all_files = list.files(pattern = '*.csv')
 all_DEU_genes = vector("list", length(all_files))
 for (i in length(all_files)){
   file = all_files[i]
+  print(file)
   data = data.table(read.csv(file, sep='\t'))
   all_DEU_genes[[i]] = unique(data[abs(data[[7]]) >= 2.0 & data[[6]] <= 0.05, 'groupID'][[1]])
 }
   
-all_DEU_genes = Reduce(union, all_DEU_genes)
+all_DEU_genes = unique(Reduce(union, all_DEU_genes))
 all_DEU_genes = paste(all_DEU_genes, collapse = "';")
 print(all_DEU_genes)
