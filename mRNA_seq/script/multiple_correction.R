6c94f7f7f7a2562f72a79298c5983fb11b6445f3
trangdht <trang@ki-elements.de>
2021-07-26 11:02:35 +0200

    Filter by exon

--- mRNA_seq/script/multiple_correction.R
+++ mRNA_seq/script/multiple_correction.R
@@ -1,7 +1,7 @@
 library(data.table, quietly=TRUE)
 library(dplyr, quietly=TRUE)
 
-setwd("/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/res")
+setwd("/Users/dhthutrang/Documents/BIOINFO/Episplicing/ENCODE_episplicing/mRNA_seq/script/temp")
 all_files = list.files(pattern = '*.csv')
 
 all_DEU_genes = vector("list", length(all_files))
@@ -9,10 +9,14 @@ for (i in 1:length(all_files)){
   file = all_files[i]
   print(file)
   data = data.table(read.csv(file, sep='\t'))
-  all_DEU_genes[[i]] = unique(data[abs(data[[7]]) >= 2.0 & data[[6]] <= 0.0001, 'groupID'][[1]])
+  print(data)
+  sig_exon = data[abs(data[[7]]) >= 2.0 & data[[6]] <= 0.0001, c('groupID', 'featureID')]
+  sig_exon = sig_exon %>% 
+    mutate(id = paste(groupID, featureID, sep=';')) %>%
+    select(id)
+  all_DEU_genes[[i]] = sig_exon$id
 }
-  
-all_DEU_genes = unique(Reduce(intersect, all_DEU_genes))
+all_DEU_genes = unique(Reduce(intersect, all_DEU_genes))[[1]]
 all_DEU_genes = all_DEU_genes[grep('+', all_DEU_genes, fixed = TRUE, invert = TRUE)]
 all_DEU_genes = all_DEU_genes[grep('_', all_DEU_genes, fixed = TRUE, invert = TRUE)]
 
@@ -23,4 +27,4 @@ all_DEU_genes = intersect(all_DEU_genes, ref_genes)
 print('After intersect: ')
 print(length(all_DEU_genes))
 
-print(paste(all_DEU_genes, collapse = "';"))
+# print(paste(all_DEU_genes, collapse = "';"))
