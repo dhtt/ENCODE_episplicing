0bc0352d8dd1d7e2e4f89fcfc4dbf2840cce6d57
trangdht <trang@ki-elements.de>
2021-07-26 20:46:49 +0200

    multiple_correction.R

--- mRNA_seq/script/multiple_correction.R
+++ mRNA_seq/script/multiple_correction.R
@@ -17,7 +17,7 @@ all_files = list.files(pattern = '*.csv')
 #}
 #all_DEU_genes = Reduce(c, all_DEU_genes)
 #saveRDS(all_DEU_genes, "all_DEU_genes.RDS")
-all_DEU_genes = readRDS("all_DEU_genes.RDS")
+# all_DEU_genes = readRDS("all_DEU_genes.RDS")
 
 freq = table(all_DEU_genes)
 freq = freq[order(freq, decreasing=TRUE)]
@@ -25,17 +25,21 @@ print("SUMMARY")
 print(summary(freq))
 print("HEAD")
 print(head(freq))
-tiff("ecdf.pdf") 
-plot(ecdf(freq))
-dev.off()
+# tiff("ecdf.pdf") 
+# plot(ecdf(freq))
+# dev.off()
 #tiff("ecdf.pdf") 
 #plot(hist(freq))
 #dev.off()
 
-all_DEU_genes = unique(all_DEU_genes)
+freq = freq[names(freq)[grep('+', names(freq), fixed = TRUE, invert = TRUE)]]
+freq = freq[names(freq)[grep('_', names(freq), fixed = TRUE, invert = TRUE)]]
+filtered_exon = names(freq[1:round(length(freq)*0.2)])
+
+all_DEU_genes = intersect(unique(all_DEU_genes), filtered_exon)
 all_DEU_genes = unique(unlist(lapply(all_DEU_genes, function(x) strsplit(x, split = ';')[[1]][1])))
-all_DEU_genes = all_DEU_genes[grep('+', all_DEU_genes, fixed = TRUE, invert = TRUE)]
-all_DEU_genes = all_DEU_genes[grep('_', all_DEU_genes, fixed = TRUE, invert = TRUE)]
+# all_DEU_genes = all_DEU_genes[grep('+', all_DEU_genes, fixed = TRUE, invert = TRUE)]
+# all_DEU_genes = all_DEU_genes[grep('_', all_DEU_genes, fixed = TRUE, invert = TRUE)]
 
 print('Before intersect: ')
 print(length(all_DEU_genes))
@@ -44,4 +48,6 @@ all_DEU_genes = intersect(all_DEU_genes, ref_genes)
 print('After intersect: ')
 print(length(all_DEU_genes))
 
-#print(paste(all_DEU_genes, collapse = "';"))
+sink("multiple_correction_genes.txt")
+print(paste(all_DEU_genes, collapse = "';"))
+sink()
