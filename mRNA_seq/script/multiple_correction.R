84e3265bd5e3ed6257bac2d030dee541d5c61ba8
Trang Do Hoang Thu <trangdo@client0279.vpn.uni-saarland.de>
2021-09-20 15:39:03 +0200

    Filter genes

--- mRNA_seq/script/multiple_correction.R
+++ mRNA_seq/script/multiple_correction.R
@@ -1,7 +1,9 @@
 library(data.table, quietly=TRUE)
 library(dplyr, quietly=TRUE)
+library(ggplot2, quietly=TRUE)
+library(reshape2, quietly=TRUE)
 
-# setwd("/Users/dhthutrang/Documents/BIOINFO/Episplicing/ENCODE_episplicing/mRNA_seq/script/temp")
+setwd("/Users/trangdo/Documents/Episplicing/ENCODE_episplicing/mRNA_seq/script/")
 setwd("/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/res")
 all_files = list.files(pattern = '*.csv')
 
@@ -19,8 +21,11 @@ all_files = list.files(pattern = '*.csv')
 #saveRDS(all_DEU_genes, "all_DEU_genes.RDS")
 all_DEU_genes = readRDS("all_DEU_genes.RDS")
 
+length(all_DEU_genes)
 freq = table(all_DEU_genes)
 freq = freq[order(freq, decreasing=TRUE)]
+freq = freq[grep('+', names(freq), fixed = T, invert = T)] 
+freq = freq[grep('_', names(freq), fixed = T, invert = T)] 
 print("SUMMARY")
 print(summary(freq))
 print("HEAD")
@@ -32,24 +37,83 @@ print(head(freq))
 #plot(hist(freq))
 #dev.off()
 
+freq_gene = unlist(lapply(names(freq), function(x) strsplit(x, split = ';')[[1]][1]))
+freq_exon = unlist(lapply(names(freq), function(x) strsplit(x, split = ';')[[1]][2]))
+freq_table = data.table(freq_exon = as.numeric(freq), gene = freq_gene, exon = freq_exon) %>%
+  dplyr::group_by(gene) %>%
+  dplyr::summarize(rank_avg = mean(freq_exon),
+                   rank_sum = sum(freq_exon))
+freq_table_melt = reshape2::melt(freq_table)
+
+all_DEU_genes = freq_table[order(freq_table$rank_avg, decreasing = T), "gene"][[1]]
+paste(all_DEU_genes[1 : round(length(all_DEU_genes)*0.25)+1], collapse = ',')
+paste(all_DEU_genes[(round(length(all_DEU_genes)*0.25)+1) : length(all_DEU_genes)], collapse = ',')
+length(all_DEU_genes[(round(length(all_DEU_genes)*0.25)+1) : length(all_DEU_genes)] )
+
+tiff("ecdf_dexseq_correction.tiff", res=300, units = "in", width=5, height = 5)
+ggplot(freq_table_melt[freq_table_melt$variable == "rank_avg", ], aes(value, col=variable, legend=F)) +
+  stat_ecdf(pad = F, na.rm = T) +
+  scale_color_manual(values = "blue") +
+  geom_vline(xintercept = 12.5, color="grey", linetype="dashed") + 
+  geom_hline(yintercept = 0.75, color='red') +
+  theme_light() +
+  theme(legend.position = "none")
+dev.off()
+
+top_25_high = freq_table %>% dplyr::top_frac(0.25, rank_avg) %>% select(gene) %>% unlist()
+top_75_low = freq_table %>% dplyr::top_frac(-0.75, rank_avg) %>% select(gene) %>% unlist()
+paste(top_75_low, collapse = ',')
+
+sink("multiple_correction_genes_75low.txt")
+print(paste(top_75_low, collapse = ","))
+sink()
+sink("multiple_correction_genes_25high.txt")
+print(paste(top_25_high, collapse = ","))
+sink()
+#==================
+
+all_genes_sig = unlist(lapply(all_DEU_genes, function(x) strsplit(x, split = ';')[[1]][1]))
+all_genes_sig = all_genes_sig[grep('+', all_genes_sig, fixed = T, invert = TRUE)]
+all_genes_sig = all_genes_sig[grep('_', all_genes_sig, fixed = T, invert = TRUE)]
+print('All genes with at least 1 sig DEU: ')
+print(length(all_genes_sig))
+all_genes_sig_freq = table(all_genes_sig)
+all_genes_sig_freq = all_genes_sig_freq[order(all_genes_sig_freq, decreasing=TRUE)]
+round(length(all_genes_sig_freq)*0.1)
+tail(all_genes_sig_freq)
+paste(names(all_genes_sig_freq[1:round(length(all_genes_sig_freq)*0.1)]), collapse=',')
+paste(names(all_genes_sig_freq[round(length(all_genes_sig_freq)*0.9): length(all_genes_sig_freq)]), collapse=',')
+
+perc=0.1
 freq = freq[names(freq)[grep('+', names(freq), fixed = TRUE, invert = TRUE)]]
 freq = freq[names(freq)[grep('_', names(freq), fixed = TRUE, invert = TRUE)]]
-filtered_exon = names(freq[1:round(length(freq)*0.1)])
+filtered_exon_1 = names(freq[1 : round(length(freq)*perc)])
+filtered_exon_9 = names(freq[round(length(freq)*perc)+1 : length(freq)])
+filtered_exon_1_low = names(freq[freq == 1])
 
-print(freq[filtered_exon])
+get_name = function(gene_exon_id){
+  return(unlist(lapply(gene_exon_id, function(x) strsplit(x, split = ';')[[1]][1])))
+}
+filtered_exon_1 = get_name(filtered_exon_1)
+filtered_exon_9 = get_name(filtered_exon_9)
+filtered_exon_1_low = get_name(filtered_exon_1_low)
 
-all_DEU_genes = intersect(unique(all_DEU_genes), filtered_exon)
-all_DEU_genes = unique(unlist(lapply(all_DEU_genes, function(x) strsplit(x, split = ';')[[1]][1])))
+length(setdiff(filtered_exon_1, filtered_exon_1_low))
+length(intersect(all_genes_sig, filtered_exon_1_low))
+"TSPAN3" %in% filtered_exon_1
+
+print(paste(setdiff(filtered_exon_1, filtered_exon_1_low), collapse = ','))
 # all_DEU_genes = all_DEU_genes[grep('+', all_DEU_genes, fixed = TRUE, invert = TRUE)]
 # all_DEU_genes = all_DEU_genes[grep('_', all_DEU_genes, fixed = TRUE, invert = TRUE)]
 
 print('Before intersect: ')
-print(length(all_DEU_genes))
-ref_genes = readRDS('/home/dhthutrang/ENCODE/mRNA_seq/script/all_genes.2021.RDS') 
-all_DEU_genes = intersect(all_DEU_genes, ref_genes)
+print(length(filtered_exon_1))
+ref_genes = readRDS('all_genes.2021.RDS') 
+all_DEU_genes_sig = intersect(filtered_exon_1, ref_genes)
 print('After intersect: ')
-print(length(all_DEU_genes))
+print(length(all_DEU_genes_sig))
 
 sink("/home/dhthutrang/ENCODE/mRNA_seq/script/multiple_correction_genes.txt")
-print(paste(all_DEU_genes, collapse = ","))
+print(paste(all_DEU_genes_sig, collapse = ","))
 sink()
+length(ref_genes)
--- mRNA_seq/script/process_multiple_dexseq.R
+++ mRNA_seq/script/process_multiple_dexseq.R
@@ -1,17 +1,69 @@
 library("Cairo")
+setwd("/Users/trangdo/Documents/Episplicing/ENCODE_episplicing/mRNA_seq/script")
 
-res_path = "/Users/trangdo/Documents/Episplicing/ENCODE_episplicing/mRNA_seq/res_10perc/dxd.res.RDS"
-html_path = "/Users/trangdo/Documents/Episplicing/ENCODE_episplicing/mRNA_seq/res_10perc/dxd.res.html"
-dxd.res = readRDS(res_path)
+# res_path_90 = "/Users/trangdo/Documents/Episplicing/ENCODE_episplicing/mRNA_seq/res_90perc/dxd.res_90.RDS"
+# html_path_90 = "/Users/trangdo/Documents/Episplicing/ENCODE_episplicing/mRNA_seq/res_90perc/dxd.res_90.html" 
+all_DEU_exons = readRDS("all_DEU_genes.RDS") 
+all_DEU_exons = unique(gsub(';', ':', all_DEU_exons))
+all_DEU_genes = unique(sapply(all_DEU_exons, function(x) strsplit(x, split=':')[[1]][1]))
+all_DEU_genes = all_DEU_genes[grep("+", all_DEU_genes, invert = T, fixed = T)]
+all_DEU_genes = all_DEU_genes[grep("_", all_DEU_genes, invert = T, fixed = T)]
+no_deu_gene =  length(all_DEU_genes)
+no_deu_exon =  length(all_DEU_exons)
+
+res_path_10 = "/Users/trangdo/Documents/Episplicing/ENCODE_episplicing/mRNA_seq/res_90perc/dxd.res_90.RDS"
+# html_path_10 = "/Users/trangdo/Documents/Episplicing/ENCODE_episplicing/mRNA_seq/res_10perc/dxd.res.html"
+dxd.res = readRDS(res_path_10)
 res_df = data.frame(dxd.res)
-colnames(res_df)
-all_exons = paste(res_df$groupID, res_df$featureID, sep=':')
-sig_exons = unique(res_df[res_df$padj < 0.05 & !is.na(res_df$padj), c('groupID', 'featureID')])
-sig_exons = paste(sig_exons$groupID, sig_exons$featureID, sep=':')
-length(all_exons)
-length(sig_exons)
-setdiff(all_exons, sig_exons)
+# all_exons = rownames(res_df)
+
+get_all_genes = function(df){
+  #' Check number of all genes used in DEXSeq global
+  all_genes_ = unique(df$groupID)
+  print(paste("Number of investigated genes:", length(all_genes_)))
+  print(head(all_genes_, 10))
+  return(all_genes_)
+}
+all_corrected_genes = get_all_genes(res_df)
+no_corrected_genes = length(all_corrected_genes)
+
+get_sig_exon = function(df, file_name){
+  #' Get significant DEU from global correction
+  sig_exon_ = unique(df[df$padj < 0.05 & !is.na(df$padj), c('groupID', 'featureID')])
+  sig_gene_ = unique(sig_exon_$groupID)
+  sig_exon_ = paste(sig_exon_$groupID, sig_exon_$featureID, sep=':')
+  no_sig_exon = length(sig_exon_)
+  no_sig_gene = length(sig_gene_)
+  
+  #Exon
+  glob_sep_overlap = intersect(sig_exon_, all_DEU_exons)
+  glob_sep_union = union(sig_exon_, all_DEU_exons)
+  glob_vs_sep = setdiff(sig_exon_, all_DEU_exons)
+  sep_vs_glob = setdiff(all_DEU_exons, sig_exon_)
+  #Gene
+  glob_sep_overlap_gene = intersect(sig_gene_, all_DEU_genes)
+  glob_vs_sep_gene = setdiff(sig_gene_, all_DEU_genes)
+  sep_vs_glob_gene = setdiff(all_DEU_genes, sig_gene_)
+  
+  print(paste("Number of significant exon:", no_sig_exon))
+  print(paste("Number of significant gene:", no_sig_gene))
+  print(paste("Number of corrected exon:", no_deu_exon))
+  print(paste("Number of corrected gene:", no_deu_gene))
+  
+  print(paste("DEU rate exon (Global sig/Pooled sig):", no_sig_exon/no_deu_exon))
+  print(paste("DEU rate gene (Global sig/Pooled sig):", no_sig_gene/no_deu_gene))
+  print(paste("Overlap - In glob - In Pooled", 
+               paste(length(glob_sep_overlap), length(glob_vs_sep), length(sep_vs_glob), sep=' - ')))
+  print(paste("Overlap/Union:", length(glob_sep_overlap)/length(glob_sep_union)))
+  saveRDS(sig_exon_, file_name)
+  return(glob_sep_overlap)
+}
+sig_exons = get_sig_exon(res_df, "multiple_correction_sig_exon_75low.RDS")
 
 DEXSeqHTML(dxd.res, genes='ACD', 
+           path = html_path,
+           FDR=0.05)
+
+DEXSeqHTML(dxd.res, genes='FGFR2', 
            path = html_path,
            FDR=0.05)
