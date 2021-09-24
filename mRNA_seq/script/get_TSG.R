36344ccf01aa1bd1ac03ae6c3486008c4987d053
Trang Do Hoang Thu <trangdo@client0519.vpn.uni-saarland.de>
2021-09-25 01:22:01 +0200

    get tissue spec genes

--- mRNA_seq/script/get_TSG.R
+++ mRNA_seq/script/get_TSG.R
@@ -19,15 +19,30 @@ normed_count = as.data.frame(normed_count)
 
 normed_count_scaled = as.data.frame(t(apply(normed_count, 1, function(x) (x/max(x)))))
 normed_count_scaled[is.na(normed_count_scaled)] = 0
-head(normed_count_scaled, 10)
-TSI = as.data.frame(apply(normed_count_scaled, 1, function(x)(sum(1 - x/max(x))/(length(x) -1))))
-colnames(TSI) = 'value'
-TSI$gene_id = df$groupID
-TSI$exon_id = df$featureID
-head(TSI, 10)
-saveRDS(TSI, '~/ENCODE/mRNA_seq/script/TSI.RDS')
+saveRDS(normed_count_scaled, "~/ENCODE/mRNA_seq/script/normed_count_scaled.RDS")
 
-TSI_res = TSI %>% filter(value > 0.95) %>% select(gene_id) %>% unique() %>% unlist()
-saveRDS(TSI_res, '~/ENCODE/mRNA_seq/script/TSG.RDS')
-print(paste('Number of TS gene', length(unique(TSI$gene_id)) - length(TSI_res)))
+#Gene wise
+TSG = normed_count_scaled
+TSG$gene_id = df$groupID
+TSG = TSG %>% 
+  dplyr::group_by(gene_id) %>%
+  dplyr::summarise_all(mean(., na.rm=T))
+TSG_mat = TSG %>% dplyr::select(-gene_id)
+TSG_mat = as.data.frame(apply(TSG_mat, 1, function(x)(sum(1 - x/max(x))/(length(x) -1))))
+TSG_mat$gene_id = TSG$gene_id
+TSG_gene = TSG_mat$gene_id[TSG_mat >= 0.75]
+saveRDS(TSG_gene, '~/ENCODE/mRNA_seq/script/TSG_gene.RDS')
 
+# #Exon wise
+# TSI = as.data.frame(apply(normed_count_scaled, 1, function(x)(sum(1 - x/max(x))/(length(x) -1))))
+# colnames(TSI) = 'value'
+# TSI$gene_id = df$groupID
+# TSI$exon_id = df$featureID
+# head(TSI, 10)
+# saveRDS(TSI, '~/ENCODE/mRNA_seq/script/TSI.RDS')
+# 
+# TSI_res = TSI %>% filter(value > 0.95) %>% select(gene_id) %>% unique() %>% unlist()
+# saveRDS(TSI_res, '~/ENCODE/mRNA_seq/script/TSG.RDS')
+# print(paste('Number of TS gene', length(unique(TSI$gene_id)) - length(TSI_res)))
+# 
+# 
