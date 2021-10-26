fa6bbe8fade6c745056ca124191d8dfe96cb9e4f
Trang Do Hoang Thu <trangdo@client0597.vpn.uni-saarland.de>
2021-10-26 16:31:59 +0200

    get_cor2

--- utilities/get_cor2.R
+++ utilities/get_cor2.R
@@ -68,9 +68,9 @@ get_all_pairs.his <- function(all_pairs.his, his){
     pair.his = pair.his %>%
       dplyr::mutate(
         gene = id$gene, exon = id$exon, type = V3,
-        p_val = as.numeric(as.character(V14)),
+        p_val = as.numeric(as.character(V11)),
         m_val = dplyr::if_else(p_val <= 0.05, 
-                               true = abs(as.numeric(as.character(V13))), false = 0)
+                               true = abs(as.numeric(as.character(V10))), false = 0)
       ) %>%
       dplyr::select(p_val)
       dplyr::ungroup() 
