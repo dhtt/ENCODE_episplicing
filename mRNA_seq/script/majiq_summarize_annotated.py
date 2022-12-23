05d444bf665371067bbfee690a932f1fbf22d215
dhtt_uni <dhthutrang@giga1.bioinformatik.uni-saarland.de>
2022-12-23 13:15:41 +0100

    Clean up unnecessary files

--- mRNA_seq/script/majiq_summarize_annotated.py
+++
@@ -1,9 +0,0 @@
-
-
-if __name__ == "__main__":
-    his_annotated_file = "/Users/dhthutrang/Documents/BIOINFO/Episplicing/ENCODE_episplicing/majiq/temp.tsv"
-    with open(his_annotated_file) as reader:
-        lines = [line.rstrip().split('\t') for line in reader]
-
-    LSV_info = defaultdict()
-    for i, line in enumerate(lines):
