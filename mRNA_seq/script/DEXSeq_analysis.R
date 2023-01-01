41066e68fdc5743cca5a82a992e8d578aa637499
dhtt_uni <dhthutrang@giga1.bioinformatik.uni-saarland.de>
2023-01-01 16:45:25 +0100

    Refactor & Add documentation

--- mRNA_seq/script/DEXSeq_analysis.R
+++ mRNA_seq/script/DEXSeq_analysis.R
@@ -1,96 +1,134 @@
-start_time <- Sys.time()
-#===== LOAD PACKAGES ======
-library('stringr', quietly=TRUE)
-library("data.table", quietly=TRUE)
-library("dplyr", quietly=TRUE)
-library("DEXSeq", quietly=TRUE)
-library("optparse", quietly=TRUE)
-library("BiocParallel")
-suppressPackageStartupMessages( library( "DEXSeq" ) )
+## ---------------------------
+##
+## Script name: DEXSeq_analysis.R
+##
+## Purpose of script: Perform pairwise Differential Exon Usage analysis using DEXSeq R package
+##
+## Author: Trang Do
+##
+## Copyright (c) Trang Do, 2018
+## Email: dhttrang@bioinformatik.uni-saarland.de
+##
+## ---------------------------
+##
+## Notes:
+## - Description:This script was used for pairwise DEU analysis. For multiple correction, 
+##   DEXSeq_analysis_combined.R was used. Here, 2 samples are compared against each other using 
+##   DEXSeq workflow. The script was called from execute_dexseq.sh for all possible sample pairs
+## - Preceeding script: count_exon.sh, get_all_pairs.sh
+## - Succeeding script: multiple_correction.R
+##
+## ---------------------------
 
-option_list = list(
-  make_option(c("-f", "--countfolder"), type="character", default="/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount",
-              help="path to folder of counts", metavar="character"),
-  make_option(c("-a", "--epigenome1"), type="character", default=NULL,
-              help="ID of first epigenome", metavar="character"),
-  make_option(c("-b", "--epigenome2"), type="character", default=NULL,
-              help="ID of second epigenome", metavar="character"),
-  make_option(c("-g", "--referencegenome"), type="character", default="/home/dhthutrang/ENCODE/refgen/reference_genome.gtf",
-              help="path to flattened reference genome", metavar="character"),
-  make_option(c("-n", "--numcores"), type="integer", default=1,
-              help="number of processing cores", metavar="character")
+
+start_time <- Sys.time()
+library("stringr", quietly = TRUE)
+library("data.table", quietly = TRUE)
+library("dplyr", quietly = TRUE)
+library("DEXSeq", quietly = TRUE)
+library("optparse", quietly = TRUE)
+library("BiocParallel", quietly = TRUE)
+
+
+# ==== PREPARATION ====
+# Parse arguments for input/output settings
+option_list <- list(
+  make_option(c("-f", "--countfolder"),
+    type = "character",
+    help = "path to folder of counts. $ENCODE_EXP/dexseqcount was used", metavar = "character"
+  ),
+  make_option(c("-a", "--epigenome1"),
+    type = "character", default = NULL,
+    help = "ID of first epigenome", metavar = "character"
+  ),
+  make_option(c("-b", "--epigenome2"),
+    type = "character", default = NULL,
+    help = "ID of second epigenome", metavar = "character"
+  ),
+  make_option(c("-g", "--referencegenome"),
+    type = "character",
+    help = "path to flattened reference genome. $REFGEN/reference_genome.gtf was used", metavar = "character"
+  ),
+  make_option(c("-n", "--numcores"),
+    type = "integer", default = 1,
+    help = "number of processing cores", metavar = "character"
+  )
 )
 
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
 
-#===== PREPARE DATA =====
 setwd(opt$countfolder)
-inDir = normalizePath(getwd())
-epi_id1 = opt$epigenome1
-epi_id2 = opt$epigenome2
-pair = paste(paste('^', epi_id1, ".*count.txt$", sep=''), paste('^',  epi_id2, ".*count.txt$", sep=''), sep='|')
-count_files = list.files(inDir, pattern=pair, full.names=TRUE)
-file_names = as.data.table(str_split_fixed(basename(count_files), "\\_", 3))
-gtf_files = opt$referencegenome
-# log_name = file(paste(paste(epi_id1, epi_id2, sep='_'), "log", sep='.'), open = "wt")
-cores = MulticoreParam(opt$numcores)
+inDir <- normalizePath(getwd())
+epi_id1 <- opt$epigenome1
+epi_id2 <- opt$epigenome2
+pair <- paste(paste("^", epi_id1, ".*count.txt$", sep = ""), paste("^", epi_id2, ".*count.txt$", sep = ""), sep = "|")
+count_files <- list.files(inDir, pattern = pair, full.names = TRUE)
+file_names <- as.data.table(str_split_fixed(basename(count_files), "\\_", 3))
+gtf_files <- opt$referencegenome
+cores <- MulticoreParam(opt$numcores)
 
-# sink(log_name, type = c("output", "message"))
+# Write logs
+log_name <- file(paste(paste(epi_id1, epi_id2, sep='_'), "log", sep='.'), open = "wt")
+sink(log_name, type = c("output", "message"))
+
+cat(paste("---> Working folder: ", opt$countfolder, sep=''), append = TRUE)
+cat("\n---> Count files: ", append = TRUE)
+cat(basename(count_files), append = TRUE)
+cat(paste("\n---> Reference genome: ", gtf_files, sep=''), append = TRUE)
 
 
-# cat(paste("---> Working folder: ", opt$countfolder, sep=''), append = TRUE)
-# cat("\n---> Count files: ", append = TRUE)
-# cat(basename(count_files), append = TRUE)
-# cat(paste("\n---> Reference genome: ", gtf_files, sep=''), append = TRUE)
-
-sampleTable = data.frame(
+# ===== RUN DEXSEQ =====
+# Prepare sample table
+cat("\n---> Preparing sample table", append = TRUE)
+sampleTable <- data.frame(
   row.names = c(file_names$V2),
-  condition = c(file_names$V1))
+  condition = c(file_names$V1)
+)
 
-#===== RUN DEXSEQ =====
-# cat("\n---> Inputting to DEXSeq", append = TRUE)
-dxd = DEXSeqDataSetFromHTSeq(
+
+# Start DEXSeq analysis
+cat("\n---> Inputting to DEXSeq", append = TRUE)
+dxd <- DEXSeqDataSetFromHTSeq(
   count_files,
   sampleData = sampleTable,
   design = ~ sample + exon + condition:exon,
-  flattenedfile= normalizePath(gtf_files)
+  flattenedfile = normalizePath(gtf_files)
 )
 
-# cat("\n---> Getting DEXSeq result", append = TRUE)
-dxd.res = DEXSeq(dxd, quiet = FALSE, BPPARAM=cores)
-
-#===== SAVING RESULTS =====
-# cat("\n---> Saving DEXSeq normalized counts", append = TRUE)
-dxd.count = data.frame(cbind(dxd.res[c(1,2)], counts(dxd.res, normalized = TRUE)))
-colnames(dxd.count) = c("groupID", "featureID", paste(file_names$V1, file_names$V2, sep='_'))
-normedcount_name = paste(paste(epi_id1, epi_id2, sep='_'), "normedcount.csv", sep='_')
-# write.table(dxd.count, normedcount_name, quote=FALSE, sep="\t", dec=".", row.names=FALSE, col.names=TRUE)
-# dxd.count = read.csv("temp_count.csv", header=TRUE, sep = ",")
-
-# cat("\n---> Saving DEXSeq result", append = TRUE)
-r_data_name = paste(
-  "/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/Rdata", 
-  paste(paste(epi_id1, epi_id2, sep='_'), "RData", sep='.'), 
-  sep='/')
-save(dxd.res, file=r_data_name)
-
-result_name = paste(paste(epi_id1, epi_id2, sep='_'), "res.csv", sep='_')
-# write.table(as.data.frame(dxd.res[c(1,2,3,5,6,7,10)]), result_name,
-#             quote=FALSE, sep="\t", dec=".", row.names=FALSE, col.names=TRUE)
-# dxd.res = read.csv(result_name, header=TRUE, sep = ",")
-
-#print("---> Exporting HTML DEXSeq result")
-#html_name = paste(paste(epi_id1, epi_id2, sep='_'), "html", sep='_')
-#DEXSeqHTML(dxd.res, 
-#           path = html_name,
-#           FDR=0.05, color=c("#FF000080", "#0000FF80"),
-#           BPPARAM = cores)
-
-# cat("\n===> FINISHED!", append = TRUE)
-# end_time <- Sys.time()
-# cat(paste("\nTotal time:", end_time - start_time, sep = ' '), append = TRUE)
+cat("\n---> Getting DEXSeq result", append = TRUE)
+dxd.res <- DEXSeq(dxd, quiet = FALSE, BPPARAM = cores)
 
 
-# sink()
+# Save DEXSeq results
+cat("\n---> Saving DEXSeq normalized counts", append = TRUE)
+dxd.count <- data.frame(cbind(dxd.res[c(1, 2)], counts(dxd.res, normalized = TRUE)))
+colnames(dxd.count) <- c("groupID", "featureID", paste(file_names$V1, file_names$V2, sep = "_"))
+normedcount_name <- paste(paste(epi_id1, epi_id2, sep = "_"), "normedcount.csv", sep = "_")
+write.table(dxd.count, normedcount_name, quote=FALSE, sep="\t", dec=".", row.names=FALSE, col.names=TRUE)
 
+cat("\n---> Saving DEXSeq result", append = TRUE)
+r_data_name <- paste(
+  "/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/Rdata",
+  paste(paste(epi_id1, epi_id2, sep = "_"), "RData", sep = "."),
+  sep = "/"
+)
+save(dxd.res, file = r_data_name)
+
+result_name <- paste(paste(epi_id1, epi_id2, sep = "_"), "res.csv", sep = "_")
+write.table(as.data.frame(dxd.res[c(1,2,3,5,6,7,10)]), result_name,
+            quote=FALSE, sep="\t", dec=".", row.names=FALSE, col.names=TRUE)
+dxd.res = read.csv(result_name, header=TRUE, sep = ",")
+
+print("---> Exporting HTML DEXSeq result")
+html_name = paste(paste(epi_id1, epi_id2, sep='_'), "html", sep='_')
+DEXSeqHTML(dxd.res,
+          path = html_name,
+          FDR=0.05, color=c("#FF000080", "#0000FF80"),
+          BPPARAM = cores)
+
+cat("\n===> FINISHED!", append = TRUE)
+end_time <- Sys.time()
+cat(paste("\nTotal time:", end_time - start_time, sep = ' '), append = TRUE)
+
+sink()
--- mRNA_seq/script/DEXSeq_analysis_combined.R
+++ mRNA_seq/script/DEXSeq_analysis_combined.R
@@ -1,100 +1,99 @@
-start_time <- Sys.time()
-#===== LOAD PACKAGES ======
-library('stringr', quietly=TRUE)
-library("data.table", quietly=TRUE)
-library("dplyr", quietly=TRUE)
-library("DEXSeq", quietly=TRUE)
-library("optparse", quietly=TRUE)
-library("BiocParallel")
-suppressPackageStartupMessages( library( "DEXSeq" ) )
+## ---------------------------
+##
+## Script name: DEXSeq_analysis_combined.R
+##
+## Purpose of script: Perform combined Differential Exon Usage analysis using DEXSeq R package
+##
+## Author: Trang Do
+##
+## Copyright (c) Trang Do, 2018
+## Email: dhttrang@bioinformatik.uni-saarland.de
+##
+## ---------------------------
+##
+## Notes: 
+## - Description: This script was used for multiple correction of DEU analysis. For pairwise 
+## comparison, DEXSeq_analysis.R was used
+## - Preceeding script: - multiple_correction.R
+## - Succeeding script: - process_multiple_dexseq.R
+## ---------------------------
 
-option_list = list(
-  make_option(c("-f", "--countfolder"), type="character", default="/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/correction/count",
-              help="path to folder of counts", metavar="character"),
-  make_option(c("-g", "--referencegenome"), type="character", default="/home/dhthutrang/ENCODE/refgen/reference_genome.gtf",
-              help="path to flattened reference genome", metavar="character"),
-  make_option(c("-n", "--numcores"), type="integer", default=1,
-              help="number of processing cores", metavar="character")
+
+start_time <- Sys.time()
+library("stringr", quietly = TRUE)
+library("data.table", quietly = TRUE)
+library("dplyr", quietly = TRUE)
+library("DEXSeq", quietly = TRUE)
+library("optparse", quietly = TRUE)
+library("BiocParallel", quietly = TRUE)
+
+
+# ==== PREPARATION ====
+# Parse arguments for input/output settings
+option_list <- list(
+  make_option(c("-f", "--countfolder"),
+    type = "character", default = "/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/correction/count",
+    help = "path to folder of counts", metavar = "character"
+  ),
+  make_option(c("-g", "--referencegenome"),
+    type = "character", default = "/home/dhthutrang/ENCODE/refgen/reference_genome.gtf",
+    help = "path to flattened reference genome", metavar = "character"
+  ),
+  make_option(c("-n", "--numcores"),
+    type = "integer", default = 1,
+    help = "number of processing cores", metavar = "character"
+  )
 )
 
-opt_parser = OptionParser(option_list=option_list)
-opt = parse_args(opt_parser)
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
 
-#===== PREPARE DATA =====
 setwd(opt$countfolder)
-inDir = normalizePath(getwd())
-count_files = list.files(inDir, pattern='*count.txt$', full.names=TRUE)
-print("====================================")
-print("count_files")
-print(count_files)
-file_names = as.data.table(str_split_fixed(basename(count_files), "\\_", 3))
+inDir <- normalizePath(getwd())
+count_files <- list.files(inDir, pattern = "*count.txt$", full.names = TRUE)
+file_names <- as.data.table(str_split_fixed(basename(count_files), "\\_", 3))
+gtf_files <- opt$referencegenome
+cores <- MulticoreParam(opt$numcores)
 
-gtf_files = opt$referencegenome
-# log_name = file(paste(paste(epi_id1, epi_id2, sep='_'), "log", sep='.'), open = "wt")
-cores = MulticoreParam(opt$numcores)
+# Write logs
+log_name <- file(paste(paste(epi_id1, epi_id2, sep='_'), "log", sep='.'), open = "wt")
+sink(log_name, type = c("output", "message"))
 
-# sink(log_name, type = c("output", "message"))
+cat(paste("---> Working folder: ", opt$countfolder, sep=''), append = TRUE)
+cat("\n---> Count files: ", append = TRUE)
+cat(basename(count_files), append = TRUE)
+cat(paste("\n---> Reference genome: ", gtf_files, sep=''), append = TRUE)
 
 
-# cat(paste("---> Working folder: ", opt$countfolder, sep=''), append = TRUE)
-# cat("\n---> Count files: ", append = TRUE)
-# cat(basename(count_files), append = TRUE)
-# cat(paste("\n---> Reference genome: ", gtf_files, sep=''), append = TRUE)
-
-sampleTable = data.frame(
+# ===== RUN DEXSEQ =====
+# Prepare sample table
+sampleTable <- data.frame(
   row.names = c(file_names$V2),
-  condition = c(file_names$V1))
-print("====================================")
-print("sampleTable")
-print(sampleTable)
-#===== RUN DEXSEQ =====
-# cat("\n---> Inputting to DEXSeq", append = TRUE)
-dxd = DEXSeqDataSetFromHTSeq(
+  condition = c(file_names$V1)
+)
+
+# Start DEXSeq analysis
+cat("\n---> Inputting to DEXSeq", append = TRUE)
+dxd <- DEXSeqDataSetFromHTSeq(
   count_files,
   sampleData = sampleTable,
   design = ~ sample + exon + condition:exon,
-  flattenedfile= normalizePath(gtf_files)
+  flattenedfile = normalizePath(gtf_files)
 )
-saveRDS(dxd, '/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/correction/res_90perc/dxd.RDS')
-#dxd = readRDS('/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/correction/res_90perc/dxd_90.RDS')
-print(paste('dxd done: ', Sys.time()))
+saveRDS(dxd, "/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/correction/res_90perc/dxd.RDS")
+print(paste("dxd done: ", Sys.time()))
 
-# cat("\n---> Getting DEXSeq result", append = TRUE)
-dxd.res = DEXSeq(dxd, quiet = FALSE, BPPARAM=cores)
+cat("\n---> Getting DEXSeq result", append = TRUE)
+dxd.res <- DEXSeq(dxd, quiet = FALSE, BPPARAM = cores)
 print(paste("dxd.res done: ", Sys.time()))
-###save(dxd.res, file="/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/correction/dxd.res_90.RDS")
-saveRDS(dxd.res, '/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/correction/res_90perc/dxd.res.RDS')
-# dxd.res = readRDS('/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/correction/dxd.res.RDS')
-print(paste('dxd.res saved: ', Sys.time()))
 
-#===== SAVING RESULTS =====
-# cat("\n---> Saving DEXSeq normalized counts", append = TRUE)
-# dxd.count = data.frame(cbind(dxd.res[c(1,2)], counts(dxd.res, normalized = TRUE)))
-# colnames(dxd.count) = c("groupID", "featureID", paste(file_names$V1, file_names$V2, sep='_'))
-# normedcount_name = paste(paste(epi_id1, epi_id2, sep='_'), "normedcount.csv", sep='_')
-# write.table(dxd.count, normedcount_name, quote=FALSE, sep="\t", dec=".", row.names=FALSE, col.names=TRUE)
-# # dxd.count = read.csv("temp_count.csv", header=TRUE, sep = ",")
-# 
-# # cat("\n---> Saving DEXSeq result", append = TRUE)
-# r_data_name = paste(paste(epi_id1, epi_id2, sep='_'), "RData", sep='.')
-# save(dxd.res, file=r_data_name)
-# 
-# result_name = paste(paste(epi_id1, epi_id2, sep='_'), "res.csv", sep='_')
-# write.table(as.data.frame(dxd.res[c(1,2,3,5,6,7,10)]), result_name,
-#             quote=FALSE, sep="\t", dec=".", row.names=FALSE, col.names=TRUE)
-# # dxd.res = read.csv(result_name, header=TRUE, sep = ",")
-# 
-# #print("---> Exporting HTML DEXSeq result")
-# #html_name = paste(paste(epi_id1, epi_id2, sep='_'), "html", sep='_')
-# #DEXSeqHTML(dxd.res, 
-# #           path = html_name,
-# #           FDR=0.05, color=c("#FF000080", "#0000FF80"),
-# #           BPPARAM = cores)
-# 
-# # cat("\n===> FINISHED!", append = TRUE)
-# # end_time <- Sys.time()
-# # cat(paste("\nTotal time:", end_time - start_time, sep = ' '), append = TRUE)
-# 
-# 
-# # sink()
 
+# Save DEXSeq results
+saveRDS(dxd.res, "/home/dhthutrang/ENCODE/mRNA_seq/dexseqcount/correction/res_90perc/dxd.res.RDS")
+print(paste("dxd.res saved: ", Sys.time()))
+
+
+cat("\n===> FINISHED!", append = TRUE)
+end_time <- Sys.time()
+cat(paste("\nTotal time:", end_time - start_time, sep = ' '), append = TRUE)
+sink()
